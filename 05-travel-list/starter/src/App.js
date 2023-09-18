3ef7d079cfd17b35db078520c6762d67a46ac765
Xuan <xuancbblin@gmail.com>
2023-09-18 14:21:06 +0800

    feat: ch6: 70. Building the Layout

---
+++ 05-travel-list/starter/src/App.js
@@ -0,0 +1,34 @@
+export default function App() {
+  return (
+    <div className="app">
+      <Logo></Logo>
+      <Form></Form>
+      <PackingList></PackingList>
+      <States></States>
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🎁 Far Away 🎁</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 🎁 trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return <div className="list">LIST</div>;
+}
+
+function States() {
+  return (
+    <footer className="stats">
+      <em>🎉 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
