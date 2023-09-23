0a3e5d8992b32e7067d65e4193206afa2562903e
Xuan <xuancbblin@gmail.com>
2023-09-23 14:37:27 +0800

    feat: 85. Calculating Statistics as Derived State

--- 05-travel-list/starter/src/App.js
+++ 05-travel-list/starter/src/App.js
@@ -25,7 +25,7 @@ export default function App() {
       <Logo></Logo>
       <Form onHandleAddItem={handleAddItem}></Form>
       <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem}></PackingList>
-      <States></States>
+      <States items={items}></States>
     </div>
   );
 }
@@ -97,10 +97,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function States() {
+function States({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🚀</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>🎉 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
